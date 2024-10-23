702e51fe8a19123fd4a65da446e9b1bec4a85f1f
Devansh Chauhan <1100022701@edu.cbe.ab.ca>
2024-10-23 15:24:45 -0600

    starting

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require("express");
 const uuid = require("uuid")
 const server = express();
 server.use(express.json())
+server.use(express.static('public'));
 
 
 //All your code goes here
 let activeSessions={}
-
+activeSessions = {
+    someUUID:{
+        wordToGuess: 'apple',
+        guesses: [...],
+        ...
+    },
+    anotherUUID:{
+        wordToGuess: 'phase',
+        guesses: [...],
+        ...
+    }
+}
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
 module.exports = server;

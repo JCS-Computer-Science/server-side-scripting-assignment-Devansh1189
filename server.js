5467097401013833035be4ca22f99fd906576d95
Devansh Chauhan <1100022701@edu.cbe.ab.ca>
2024-10-24 13:57:07 -0600

    Identifying letters

--- server.js
+++ server.js
@@ -7,18 +7,32 @@ server.use(express.static('public'));
 
 //All your code goes here
 let activeSessions={}
-activeSessions = {
-    someUUID:{
-        wordToGuess: 'apple',
-        guesses: [...],
-        ...
-    },
-    anotherUUID:{
-        wordToGuess: 'phase',
-        guesses: [...],
-        ...
-    }
+
+//helper function to geta random word for the game/
+const words = ['apple', 'phase', 'table', 'grape', 'shine'];
+const getRandomWord = () => words [Math.floor(Math.random() * words.length)].toLowerCase();
+
+// Functionto evaluate the guess.
+const evaluateGuess = ( word, guess)=> {
+    const wordArray = word.split('');
+    const guessArray = guess.split('');
+    const result = [];
+    const closeLetters = [];
+    const rightLetters = [];
+    const wordCopy = [...wordArray];
 }
+
+ // chechking for right letters
+guess.Array.forEach((letter, index) => {
+    if (wordCopy[index]=== letter) {
+        result.push({ value: letter, result: 'RIGHT'});
+        rigthLetters.push(letter);
+        wordCopy[index]= null; 
+    } else{
+        result.push({ value: letter, result: 'WRONG'});
+    }
+});
+
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
 module.exports = server;
